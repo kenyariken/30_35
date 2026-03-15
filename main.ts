49ba8d13bc050f0d0b5ebdce22224ba22a80011f
Ken <161325958+kenyariken@users.noreply.github.com>
2026-03-15 21:51:44 +0900

    Aボタンを押してカウントを止めるように変更

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    isRunning = false
+})
+let isRunning = false
 let count = 0
-let isRunning = true
+isRunning = true
 basic.forever(function () {
     if (isRunning == true) {
         count += 1
